903d63662d573d7a495e3bfe4af2441e328d2c7a
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-09-22 17:44:12 -0700

    fix path

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 import express from "express"
 import bodyParser from "body-parser"
 import apiRouter from "./apiRouter.js"
-import favicon from 'serve-favicon'
 import path from "path"
+import url from 'url'
 
-const __dirname = path.resolve()
-console.log('resolved path', __dirname)
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-//app.use(express.static('dist'))
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static('dist'))
+
 app.use('/api', apiRouter)
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 export default app
