89d485999fe01175e32d1f872111fde38bc62837
Yevgeniy Akimenko <yakimenko@bcit.ca>
2022-09-22 17:20:42 -0700

    fix path

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from "express"
 import bodyParser from "body-parser"
 import apiRouter from "./apiRouter.js"
+import path from "path"
+
+const __dirname = path.resolve()
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('dist'))
+//app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 app.use('/api', apiRouter)
 
 app.get('/', (req, res) => {
